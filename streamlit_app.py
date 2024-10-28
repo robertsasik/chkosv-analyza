343e632e3eca9476773e7d6e9ab06e1089715a42
robertsasik <119259754+robertsasik@users.noreply.github.com>
2024-10-28 12:25:39 +0000

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,41 +6,26 @@ import plotly.express as px
 import folium
 from streamlit_folium import st_folium
 
-# Načítanie skrytých premenných z Streamlit Secrets
+#Vytvorenie skrytých premenných na pripojenie do databázy
 host = st.secrets["db_host"]
 port = int(st.secrets["db_port"])
 database = st.secrets["db_database"]
 user = st.secrets["db_user"]
 password = st.secrets["db_password"]
 
-@st.cache_resource
-def get_db_connection():
-    # Vytvorenie URL pre pripojenie k databáze
-    db_connection_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
-    
-    try:
-        # Pokus o vytvorenie pripojenia
-        engine = create_engine(db_connection_url)
-        
-        # Test pripojenia vykonaním jednoduchého dotazu
-        with engine.connect() as connection:
-             connection.execute("SELECT * FROM;")
-        
-        st.success("Pripojenie k databáze bolo úspešne nadviazané!")
-        return engine
-    
-    except OperationalError as e:
-        st.error(f"Pripojenie k databáze zlyhalo: {e}")
-    
-    finally:
-        # Ak je engine vytvorený, uvoľníme pripojenie
-        if 'engine' in locals():
-            engine.dispose()
-            st.write("Pripojenie bolo ukončené.")
+@st.cache_resource #dekorátor pripojenia na databázové zdroje
 
-# Volanie funkcie a uloženie pripojenia do premennej 'con'
+#Vytvorenie funkcie na pripojenie na databázu
+def get_db_connection():
+    db_connection_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+    engine = create_engine(db_connection_url)
+    engine.dispose() 
+    return engine
+
+#Volanie funkcie pomocou premennej con
 con = get_db_connection()
 
+st.write("Dosiel som sem")
 
 
 
